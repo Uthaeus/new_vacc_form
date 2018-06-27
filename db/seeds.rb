be668bdf9f8fed5c9ad412c7f6e3bea2799a1b69
Uthaeus <romanlavery@gmail.com>
2018-06-26 18:26:36 -0600

    Styled vaccinations forms and pages, add regular user to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
-User.create!(email: 'test@test.com', name: 'Homer Simpson', password: 'asdfasdf', password_confirmation: 'asdfasdf')
+User.create!(email: 'test@test.com', name: 'Homer Simpson', password: 'asdfasdf', password_confirmation: 'asdfasdf', role: 'admin')
 
-puts "1 User created"
+puts "1 Admin User created"
+
+User.create!(email: 'test2@test.com', name: 'Jim Beam', password: 'asdfasdf'. password_confirmation: 'asdfasdf', role: 'user')
+
+puts "1 Regular User created"
 
 vaccs_list = ['Tdap', 'Tuberculosis', 'Hep A', 'Hep B', 'Varicella', 'Rubella', 'Mumps', 'Anti-hbs', 'Rubeola']
 
